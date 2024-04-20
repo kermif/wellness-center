4cd64ff393c9a25efa15c8a5e4fa2348beadf063
kermit404 <68835493+kermit404@users.noreply.github.com>
2024-04-20 12:42:44 -0700

    edit index.html
    Co-authored-by: faaizahk <faaizahk@users.noreply.github.com>
    Co-authored-by: mahika-khare <mahika-khare@users.noreply.github.com>
    Co-authored-by: hacnguyen <amythebuddy@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+function showpopup() {
+    var popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
+  }
+function hidepopup() {
+    var popup = document.getElementById("popup");
+    popup.style.visibility = "hidden";
+}
